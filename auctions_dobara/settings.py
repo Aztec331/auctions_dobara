70aa099070881965a79ff28f828809704d99ffac
ADITYA BABAR <adityabuzz331@gmail.com>
2025-06-26 21:10:43 +0530

    added another comment

--- auctions_dobara/settings.py
+++ auctions_dobara/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 #added this comment yo
+#added another comment
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
